82605c65be2adcc67ef8ae68d2b3d97291370e66
EshetuGC <82573927+EshetuGC@users.noreply.github.com>
2021-05-03 13:18:07 -1200

    This is sample class code having four methods

---
+++ sample.java
@@ -0,0 +1,23 @@
+
+public class sample {
+	public void sample1() {
+		System.out.println("This is sample code in method one");
+	}
+	public void sample2() {
+		System.out.println("This is sample code in method one");
+	}
+	public void sample3() {
+		System.out.println("This is sample code in method one");
+	}
+	public void sample4() {
+		System.out.println("This is sample code in method one");
+	}
+	public static void main(String args[]) {
+		sample s=new sample();
+		s.sample1();
+		s.sample2();
+		s.sample3();
+		s.sample4();
+	}
+
+}
